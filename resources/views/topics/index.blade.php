ec29580dfe9d29beecb49745f897c2e18fffe598
Alex Esaki <apachecordovax@gmail.com>
2020-02-26 20:59:44 +0100

    Afficher les sujets

---
+++ resources/views/topics/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="list-group">
+            @foreach($topics as $topic)
+                <div class="list-group-item">
+                    <h4><a href="#">{{ $topic->title }}</a></h4>
+                    <p>{{ $topic->content }}</p>
+                    <div class="d-flex justify-content-between align-item-center">
+                        <small>Posté le{{ $topic->created_at->format('d/m/Y à H:m') }}</small>
+                        <span class="badge badge-primary">{{ $topic->user->name }}</span>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+        <div class="d-flex justify-content-center mt-3">
+            {{ $topics->links() }}
+        </div>
+    </div>
+@endsection
